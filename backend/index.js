64618b919e3054b5c847e186b63276d0c72e4825
julydeath <manojkarajada.mk@gmail.com>
2025-02-03 21:32:43 +0530

    chore: init websockets

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { createServer } from "node:http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
-app.listen(express.json());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello");
