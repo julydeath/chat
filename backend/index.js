68be532235e2bd34b04d7b495d6a28ec21dcb084
julydeath <manojkarajada.mk@gmail.com>
2025-02-04 17:49:56 +0530

    fix: cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,10 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://192.168.1.6:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://chat-frontend-nine-beige.vercel.app/",
+    ],
   },
 });
 
