877a7bfa1b65a811a61e25f039b1589e0d099f28
julydeath <manojkarajada.mk@gmail.com>
2025-02-04 17:13:53 +0530

    chore: url to env

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = "http://localhost:8080";
+const URL = process.env.SOCKET_URL;
 
 export const socket = io(URL);
