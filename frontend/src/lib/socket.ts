1f5049d2f034c72d6ddbb6ee80057410a49c50c1
julydeath <manojkarajada.mk@gmail.com>
2025-02-04 17:46:28 +0530

    chore: next env fix

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.SOCKET_URL;
+const URL = process.env.NEXT_PUBLIC_SOCKET_URL;
+
+console.log(URL);
 
 export const socket = io(URL);
